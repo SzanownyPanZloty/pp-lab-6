b0c3fae1c5c7f5ef6e6d5a3c7b2327add53c45b4
Jakub Zlotek <kubazlotek@gmail.com>
2024-05-05 17:49:07 +0200

    upload

--- Main.java
+++ Main.java
@@ -1,21 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import company.abstracts.Employee;
-import company.interfaces.Employable;
 import company.models.Manager;
 import company.models.Worker;
 
 public class Main {
   public static void main(String[] args) {
-    Worker worker1 = new Worker("John", 2000.0, 1);
-    Worker worker2 = new Worker("Alice", 2500.0, 2);
-    Worker worker3 = new Worker("Bob", 3000.0, 3);
-    Worker worker4 = new Worker("Eve", 3500.0, 4);
-    Manager manager = new Manager("Tom", 5000.0, 5);
+    Worker worker1 = new Worker("John", 2000.0, 1, "2021-01-01");
+    Worker worker2 = new Worker("Alice", 2500.0, 2, "2021-01-02");
+    Worker worker3 = new Worker("Bob", 3000.0, 3, "2021-01-03");
+    Worker worker4 = new Worker("Eve", 3500.0, 4, "2021-01-04");
+    Manager manager = new Manager("Tom", 5000.0, 5, "2021-01-05");
 
-    Object[] workers = { worker1, worker2, worker3, worker4, manager };
+    List<Employee> employees = new ArrayList<>();
+    employees.add(worker1);
+    employees.add(worker2);
+    employees.add(worker3);
+    employees.add(worker4);
+    employees.add(manager);
 
-    for (Object worker : workers) {
-      System.out.println("Salary: " + ((Employee) worker).getSalary());
-      ((Employable) worker).work();
+    for (Employee employee : employees) {
+      System.out.println(employee.getName() + " (ID: " + employee.getId() + "), Position: " + employee.getPosition()
+          + ", Hire date: " + employee.getHireDate() + ", Salary: " + employee.getSalary());
+      employee.work();
     }
+
   }
 }
--- company/models/Manager.java
+++ company/models/Manager.java
@@ -6,8 +6,8 @@ import company.abstracts.Employee;
 
 public class Manager extends Employee {
 
-  public Manager(String name, Double salary, Integer id) {
-    super(name, salary, id);
+  public Manager(String name, Double salary, Integer id, String hireDate) {
+    super(name, salary, id, hireDate, "Manager");
   }
 
   @Override
